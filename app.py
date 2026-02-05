ee1cc9d4e83011069f9a3dd39fb1a64bc24541b8
angelmc-12 <angelmaytacoaguila@gmail.com>
2026-02-05 07:15:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
-st.markdown("*Streamlit* is **really** ***cool***.")
+st.set_page_config(page_title="MNIST Classifier (Techy)", layout="wide")
 
-st.markdown("Holiiiiii")
+st.title("MNIST Classifier — From Scratch (CNN) + Demo")
+st.caption("Dibuja un dígito o sube una imagen. El modelo predice 0–9 y muestra probabilidades.")
 
 st.code(f"""
 import streamlit as st
