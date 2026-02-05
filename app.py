1e69b35ea0d08d507de05ef4f2ddaf60124d9ec5
angelmc-12 <angelmaytacoaguila@gmail.com>
2026-02-05 18:01:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -149,7 +149,7 @@ with left:
         else:
             canvas = st_canvas(
                 fill_color="black",
-                stroke_width=18,
+                stroke_width=8,
                 stroke_color="white",
                 background_color="black",
                 height=280,
